05c08aaa080153c361b9418490ee17335a21b2e1
Thiery <thiery.ribeiro@farfetch.com>
2024-03-14 18:09:56 +0000

    Create AirUSAData project

---
+++ AirUSAData/AirUSAData/AirUSADataApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirUSADataApp.swift
+//  AirUSAData
+//
+//  Created by Thiery Ribeiro on 14/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct AirUSADataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AirUSAData/AirUSADataUITests/AirUSADataUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AirUSADataUITestsLaunchTests.swift
+//  AirUSADataUITests
+//
+//  Created by Thiery Ribeiro on 14/03/2024.
+//
+
+import XCTest
+
+final class AirUSADataUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
